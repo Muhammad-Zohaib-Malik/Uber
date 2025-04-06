bb5fa81a6d3e1d1a19688384fd88f06ac97d3c3c
zohaib <muhammadzohaibmalik10@gmail.com>
2025-04-07 01:58:50 +0500

    connect db to local

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
+import { connectDb } from './db/db.js';
 export const app = express();
+
+connectDb();
 
 app.use(cors());
 
-
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  res.send('Hello World');
 });
-
